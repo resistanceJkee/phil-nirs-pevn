4a4a50f777967e5177fbbf34ab7f4c89681e179d
rstJkee <maximkostenko123@gmail.com>
2021-04-08 11:03:00 +0400

    Init commit

--- src/components/body/BodySchool.js
+++ src/components/body/BodySchool.js
@@ -4,9 +4,24 @@ import "./style-body.css";
 
 const BodySchool = (props) => {
     const data = props.data;
+    let i = 0;
+    let linkToAssets = "../static/assets/";
+    let extension = ".png";
     return (
         <div className="wrapper">
-            {data.map(item => (<NavLink to={item.link}>{item.nameSchool}</NavLink>))}
+            {data.map(item => (
+                <div class = 'school_item' style={{backgroundImage: "url(" + linkToAssets + i++ + extension + ")"}}>
+                    <div class="school_item_header" >
+                        <NavLink to={item.link}>{item.nameSchool}
+                        </NavLink>
+                    </div>
+                        <button class = 'school_item_button'>
+                            <NavLink to={item.link}>Подробнее
+                            </NavLink>
+                        </button>
+                    
+                </div>))}
+                
         </div>
     )
 }
